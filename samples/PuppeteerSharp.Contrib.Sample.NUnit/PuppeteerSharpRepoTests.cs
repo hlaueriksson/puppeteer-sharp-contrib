8b3b28ef913c09822e123cdcb5d0b17549cdc641
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-12-28 23:08:16 +0100

    :white_check_mark:

---
+++ samples/PuppeteerSharp.Contrib.Sample.NUnit/PuppeteerSharpRepoTests.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PuppeteerSharp.Contrib.Extensions;
+using PuppeteerSharp.Contrib.Should;
+using Shouldly;
+
+namespace PuppeteerSharp.Contrib.Sample
+{
+    public class PuppeteerSharpRepoTests
+    {
+        private Browser Browser { get; set; }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            Browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Browser.CloseAsync();
+        }
+
+        [Test]
+        public async Task Should_be_first_search_result_on_GitHub()
+        {
+            var page = await Browser.NewPageAsync();
+
+            await page.GoToAsync("https://github.com/");
+            var h1 = await page.QuerySelectorAsync("h1");
+            await h1.ShouldHaveContentAsync("Built for developers");
+
+            var input = await page.QuerySelectorAsync("input.header-search-input");
+            if (await input.IsHiddenAsync()) await page.ClickAsync(".octicon-three-bars");
+            await page.TypeAsync("input.header-search-input", "Puppeteer Sharp");
+            await page.Keyboard.PressAsync("Enter");
+            await page.WaitForNavigationAsync();
+
+            var repositories = await page.QuerySelectorAllAsync(".repo-list-item");
+            repositories.Length.ShouldBeGreaterThan(0);
+            var repository = repositories.First();
+            var link = await repository.QuerySelectorAsync("a");
+            var text = await repository.QuerySelectorAsync("p");
+            await repository.ShouldHaveContentAsync("kblok/puppeteer-sharp");
+            await text.ShouldHaveContentAsync("Headless Chrome .NET API");
+            await link.ClickAsync();
+            await page.WaitForNavigationAsync();
+
+            h1 = await page.QuerySelectorAsync("h1");
+            await h1.ShouldHaveContentAsync("kblok/puppeteer-sharp");
+            page.Url.ShouldBe("https://github.com/kblok/puppeteer-sharp");
+        }
+
+        [Test]
+        public async Task Should_have_successful_build_status()
+        {
+            var page = await Browser.NewPageAsync();
+
+            await page.GoToAsync("https://github.com/kblok/puppeteer-sharp");
+
+            var build = await page.QuerySelectorAsync("img[alt='Build status']");
+            await build.ClickAsync();
+            await page.WaitForNavigationAsync(new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation.Networkidle0 } });
+
+            var success = await page.QuerySelectorAsync(".project-build.project-build-status.success");
+            success.ShouldExist();
+        }
+
+        [Test]
+        public async Task Should_be_up_to_date_with_the_Puppeteer_version()
+        {
+            var page = await Browser.NewPageAsync();
+
+            await page.GoToAsync("https://github.com/kblok/puppeteer-sharp");
+            var puppeteerSharpVersion = await GetLatestReleaseVersion();
+
+            await page.GoToAsync("https://github.com/GoogleChrome/puppeteer");
+            var puppeteerVersion = await GetLatestReleaseVersion();
+
+            puppeteerSharpVersion.ShouldBe(puppeteerVersion);
+
+            async Task<string> GetLatestReleaseVersion()
+            {
+                var releases = await page.QuerySelectorWithContentAsync("a", "releases");
+                await releases.ClickAsync();
+                await page.WaitForNavigationAsync();
+
+                var latest = await page.QuerySelectorAsync(".release .release-header a");
+                return VersionWithoutPatch(await latest.TextContentAsync());
+
+                string VersionWithoutPatch(string version)
+                {
+                    var tokens = version.Split(".".ToCharArray());
+                    return string.Join(".", tokens.Take(2));
+                }
+            }
+        }
+    }
+}
