2c9a12b966467340e3ce68b0295c146ef51b0fd6
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2018-12-28 18:29:20 +0100

    :sparkles:

---
+++ src/PuppeteerSharp.Contrib.Extensions/PageExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace PuppeteerSharp.Contrib.Extensions
+{
+    public static class PageExtensions
+    {
+        public static async Task<ElementHandle> QuerySelectorWithContentAsync(this Page page, string selector, string regex)
+        {
+            var handle = await page.EvaluateFunctionHandleAsync(
+                @"(selector, regex) => {
+                    var elements = document.querySelectorAll(selector);
+                    return Array.prototype.find.call(elements, function(element) {
+                        return RegExp(regex).test(element.textContent);
+                    });
+                }", selector, regex) as ElementHandle;
+
+            return handle;
+        }
+
+        public static async Task<ElementHandle[]> QuerySelectorAllWithContentAsync(this Page page, string selector, string regex)
+        {
+            // https://stackoverflow.com/a/37098508
+            var arrayHandle = await page.EvaluateFunctionHandleAsync(
+                @"(selector, regex) => {
+                    var elements = document.querySelectorAll(selector);
+                    return Array.prototype.filter.call(elements, function(element) {
+                        return RegExp(regex).test(element.textContent);
+                    });
+                }", selector, regex);
+
+            var properties = await arrayHandle.GetPropertiesAsync().ConfigureAwait(false);
+            await arrayHandle.DisposeAsync().ConfigureAwait(false);
+
+            return properties.Values.OfType<ElementHandle>().ToArray();
+        }
+    }
+}
---
+++ tests/PuppeteerSharp.Contrib.Tests/Extensions/PageExtensionsTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using PuppeteerSharp.Contrib.Extensions;
+using PuppeteerSharp.Contrib.Tests.Base;
+using Xunit;
+
+namespace PuppeteerSharp.Contrib.Tests.Extensions
+{
+    public class PageExtensionsTests : PuppeteerPageBaseTest
+    {
+        protected override async Task SetUp() => await Page.SetContentAsync("<html><body><div id='foo'>Foo</div><div id='bar'>Bar</div><div id='baz'>Baz</div></body></html>");
+
+        [Fact]
+        public async Task QuerySelectorWithContentAsync_should_return_the_first_element_that_match_the_selector_and_has_the_content()
+        {
+            var foo = await Page.QuerySelectorWithContentAsync("div", "Foo");
+            Assert.Equal("foo", foo.GetAttribute("id"));
+
+            var bar = await Page.QuerySelectorWithContentAsync("div", "Ba.");
+            Assert.Equal("bar", bar.GetAttribute("id"));
+
+            var missing = await Page.QuerySelectorWithContentAsync("div", "Missing");
+            Assert.Null(missing);
+        }
+
+        [Fact]
+        public async Task QuerySelectorAllWithContentAsync_should_return_all_elements_that_match_the_selector_and_has_the_content()
+        {
+            var divs = await Page.QuerySelectorAllWithContentAsync("div", "Foo");
+            Assert.Equal(new[] { "foo" }, divs.Select(x => x.GetAttribute("id")));
+
+            divs = await Page.QuerySelectorAllWithContentAsync("div", "Ba.");
+            Assert.Equal(new[] { "bar", "baz" }, divs.Select(x => x.GetAttribute("id")));
+
+            var missing = await Page.QuerySelectorAllWithContentAsync("div", "Missing");
+            Assert.Empty(missing);
+        }
+    }
+}
