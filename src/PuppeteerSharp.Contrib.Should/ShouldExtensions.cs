27faf044ab3f7545b5a6384af5db1e9daec1b73c
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2019-01-07 18:40:28 +0100

    :recycle:

--- src/PuppeteerSharp.Contrib.Should/ShouldExtensions.cs
+++ src/PuppeteerSharp.Contrib.Should/ShouldExtensions.cs
@@ -7,9 +7,11 @@ namespace PuppeteerSharp.Contrib.Should
     {
         // Exist
 
-        public static void ShouldExist(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldExist(this ElementHandle handle, string message = null)
         {
             if (!handle.Exists()) Throw.ShouldExist(handle, message);
+
+            return handle;
         }
 
         public static void ShouldExist(this Task<ElementHandle> task, string message = null)
@@ -17,9 +19,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!task.Exists()) Throw.ShouldExist(task, message);
         }
 
-        public static void ShouldNotExist(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldNotExist(this ElementHandle handle, string message = null)
         {
             if (handle.Exists()) Throw.ShouldNotExist(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotExist(this Task<ElementHandle> task, string message = null)
@@ -34,9 +38,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.ValueAsync().ConfigureAwait(false) != value) Throw.ShouldHaveValue(handle, message);
         }
 
-        public static void ShouldHaveValue(this ElementHandle handle, string value, string message = null)
+        public static ElementHandle ShouldHaveValue(this ElementHandle handle, string value, string message = null)
         {
             if (handle.Value() != value) Throw.ShouldHaveValue(handle, message);
+
+            return handle;
         }
 
         public static void ShouldHaveValue(this Task<ElementHandle> task, string value, string message = null)
@@ -49,9 +55,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.ValueAsync().ConfigureAwait(false) == value) Throw.ShouldNotHaveValue(handle, message);
         }
 
-        public static void ShouldNotHaveValue(this ElementHandle handle, string value, string message = null)
+        public static ElementHandle ShouldNotHaveValue(this ElementHandle handle, string value, string message = null)
         {
             if (handle.Value() == value) Throw.ShouldNotHaveValue(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotHaveValue(this Task<ElementHandle> task, string value, string message = null)
@@ -66,9 +74,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!await handle.HasAttributeAsync(name).ConfigureAwait(false)) Throw.ShouldHaveAttribute(handle, message);
         }
 
-        public static void ShouldHaveAttribute(this ElementHandle handle, string name, string message = null)
+        public static ElementHandle ShouldHaveAttribute(this ElementHandle handle, string name, string message = null)
         {
             if (!handle.HasAttribute(name)) Throw.ShouldHaveAttribute(handle, message);
+
+            return handle;
         }
 
         public static void ShouldHaveAttribute(this Task<ElementHandle> task, string name, string message = null)
@@ -81,9 +91,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.HasAttributeAsync(name).ConfigureAwait(false)) Throw.ShouldNotHaveAttribute(handle, message);
         }
 
-        public static void ShouldNotHaveAttribute(this ElementHandle handle, string name, string message = null)
+        public static ElementHandle ShouldNotHaveAttribute(this ElementHandle handle, string name, string message = null)
         {
             if (handle.HasAttribute(name)) Throw.ShouldNotHaveAttribute(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotHaveAttribute(this Task<ElementHandle> task, string name, string message = null)
@@ -98,9 +110,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!await handle.HasContentAsync(content).ConfigureAwait(false)) Throw.ShouldHaveContent(handle, message);
         }
 
-        public static void ShouldHaveContent(this ElementHandle handle, string content, string message = null)
+        public static ElementHandle ShouldHaveContent(this ElementHandle handle, string content, string message = null)
         {
             if (!handle.HasContent(content)) Throw.ShouldHaveContent(handle, message);
+
+            return handle;
         }
 
         public static void ShouldHaveContent(this Task<ElementHandle> task, string content, string message = null)
@@ -113,9 +127,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.HasContentAsync(content).ConfigureAwait(false)) Throw.ShouldNotHaveContent(handle, message);
         }
 
-        public static void ShouldNotHaveContent(this ElementHandle handle, string content, string message = null)
+        public static ElementHandle ShouldNotHaveContent(this ElementHandle handle, string content, string message = null)
         {
             if (handle.HasContent(content)) Throw.ShouldNotHaveContent(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotHaveContent(this Task<ElementHandle> task, string content, string message = null)
@@ -130,9 +146,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!await handle.HasClassAsync(className).ConfigureAwait(false)) Throw.ShouldHaveClass(handle, message);
         }
 
-        public static void ShouldHaveClass(this ElementHandle handle, string className, string message = null)
+        public static ElementHandle ShouldHaveClass(this ElementHandle handle, string className, string message = null)
         {
             if (!handle.HasClass(className)) Throw.ShouldHaveClass(handle, message);
+
+            return handle;
         }
 
         public static void ShouldHaveClass(this Task<ElementHandle> task, string className, string message = null)
@@ -145,9 +163,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.HasClassAsync(className).ConfigureAwait(false)) Throw.ShouldNotHaveClass(handle, message);
         }
 
-        public static void ShouldNotHaveClass(this ElementHandle handle, string className, string message = null)
+        public static ElementHandle ShouldNotHaveClass(this ElementHandle handle, string className, string message = null)
         {
             if (handle.HasClass(className)) Throw.ShouldNotHaveClass(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotHaveClass(this Task<ElementHandle> task, string className, string message = null)
@@ -162,9 +182,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.IsHiddenAsync().ConfigureAwait(false)) Throw.ShouldBeVisible(handle, message);
         }
 
-        public static void ShouldBeVisible(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldBeVisible(this ElementHandle handle, string message = null)
         {
             if (handle.IsHidden()) Throw.ShouldBeVisible(handle, message);
+
+            return handle;
         }
 
         public static void ShouldBeVisible(this Task<ElementHandle> task, string message = null)
@@ -177,9 +199,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.IsVisibleAsync().ConfigureAwait(false)) Throw.ShouldBeHidden(handle, message);
         }
 
-        public static void ShouldBeHidden(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldBeHidden(this ElementHandle handle, string message = null)
         {
             if (handle.IsVisible()) Throw.ShouldBeHidden(handle, message);
+
+            return handle;
         }
 
         public static void ShouldBeHidden(this Task<ElementHandle> task, string message = null)
@@ -194,9 +218,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!await handle.IsSelectedAsync().ConfigureAwait(false)) Throw.ShouldBeSelected(handle, message);
         }
 
-        public static void ShouldBeSelected(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldBeSelected(this ElementHandle handle, string message = null)
         {
             if (!handle.IsSelected()) Throw.ShouldBeSelected(handle, message);
+
+            return handle;
         }
 
         public static void ShouldBeSelected(this Task<ElementHandle> task, string message = null)
@@ -209,9 +235,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.IsSelectedAsync().ConfigureAwait(false)) Throw.ShouldNotBeSelected(handle, message);
         }
 
-        public static void ShouldNotBeSelected(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldNotBeSelected(this ElementHandle handle, string message = null)
         {
             if (handle.IsSelected()) Throw.ShouldNotBeSelected(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotBeSelected(this Task<ElementHandle> task, string message = null)
@@ -226,9 +254,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!await handle.IsCheckedAsync().ConfigureAwait(false)) Throw.ShouldBeChecked(handle, message);
         }
 
-        public static void ShouldBeChecked(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldBeChecked(this ElementHandle handle, string message = null)
         {
             if (!handle.IsChecked()) Throw.ShouldBeChecked(handle, message);
+
+            return handle;
         }
 
         public static void ShouldBeChecked(this Task<ElementHandle> task, string message = null)
@@ -241,9 +271,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.IsCheckedAsync().ConfigureAwait(false)) Throw.ShouldNotBeChecked(handle, message);
         }
 
-        public static void ShouldNotBeChecked(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldNotBeChecked(this ElementHandle handle, string message = null)
         {
             if (handle.IsChecked()) Throw.ShouldNotBeChecked(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotBeChecked(this Task<ElementHandle> task, string message = null)
@@ -258,9 +290,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.IsEnabledAsync().ConfigureAwait(false)) Throw.ShouldBeDisabled(handle, message);
         }
 
-        public static void ShouldBeDisabled(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldBeDisabled(this ElementHandle handle, string message = null)
         {
             if (handle.IsEnabled()) Throw.ShouldBeDisabled(handle, message);
+
+            return handle;
         }
 
         public static void ShouldBeDisabled(this Task<ElementHandle> task, string message = null)
@@ -273,9 +307,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.IsDisabledAsync().ConfigureAwait(false)) Throw.ShouldBeEnabled(handle, message);
         }
 
-        public static void ShouldBeEnabled(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldBeEnabled(this ElementHandle handle, string message = null)
         {
             if (handle.IsDisabled()) Throw.ShouldBeEnabled(handle, message);
+
+            return handle;
         }
 
         public static void ShouldBeEnabled(this Task<ElementHandle> task, string message = null)
@@ -290,9 +326,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!await handle.IsReadOnlyAsync().ConfigureAwait(false)) Throw.ShouldBeReadOnly(handle, message);
         }
 
-        public static void ShouldBeReadOnly(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldBeReadOnly(this ElementHandle handle, string message = null)
         {
             if (!handle.IsReadOnly()) Throw.ShouldBeReadOnly(handle, message);
+
+            return handle;
         }
 
         public static void ShouldBeReadOnly(this Task<ElementHandle> task, string message = null)
@@ -305,9 +343,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.IsReadOnlyAsync().ConfigureAwait(false)) Throw.ShouldNotBeReadOnly(handle, message);
         }
 
-        public static void ShouldNotBeReadOnly(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldNotBeReadOnly(this ElementHandle handle, string message = null)
         {
             if (handle.IsReadOnly()) Throw.ShouldNotBeReadOnly(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotBeReadOnly(this Task<ElementHandle> task, string message = null)
@@ -322,9 +362,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (!await handle.HasFocusAsync().ConfigureAwait(false)) Throw.ShouldHaveFocus(handle, message);
         }
 
-        public static void ShouldHaveFocus(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldHaveFocus(this ElementHandle handle, string message = null)
         {
             if (!handle.HasFocus()) Throw.ShouldHaveFocus(handle, message);
+
+            return handle;
         }
 
         public static void ShouldHaveFocus(this Task<ElementHandle> task, string message = null)
@@ -337,9 +379,11 @@ namespace PuppeteerSharp.Contrib.Should
             if (await handle.HasFocusAsync().ConfigureAwait(false)) Throw.ShouldNotHaveFocus(handle, message);
         }
 
-        public static void ShouldNotHaveFocus(this ElementHandle handle, string message = null)
+        public static ElementHandle ShouldNotHaveFocus(this ElementHandle handle, string message = null)
         {
             if (handle.HasFocus()) Throw.ShouldNotHaveFocus(handle, message);
+
+            return handle;
         }
 
         public static void ShouldNotHaveFocus(this Task<ElementHandle> task, string message = null)
