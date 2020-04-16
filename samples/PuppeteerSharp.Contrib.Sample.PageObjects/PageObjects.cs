41972ff3872b7a8fdf50daa2863e2d5dea77878c
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-04-16 08:16:27 +0200

    :sparkles: PuppeteerSharp.Contrib.Sample.PageObjects

---
+++ samples/PuppeteerSharp.Contrib.Sample.PageObjects/PageObjects.cs
@@ -0,0 +1,73 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using PuppeteerSharp.Contrib.Extensions;
+using PuppeteerSharp.Contrib.PageObjects;
+using PuppeteerSharp.Input;
+
+namespace PuppeteerSharp.Contrib.Sample
+{
+    public class GitHubStartPage : PageObject
+    {
+        [Selector("h1")]
+        public virtual Task<ElementHandle> Heading { get; }
+        
+        [Selector(".HeaderMenu")]
+        public virtual Task<GitHubHeaderMenu> HeaderMenu { get; }
+    }
+
+    public class GitHubHeaderMenu : ElementObject
+    {
+        [Selector("input.header-search-input")]
+        public virtual Task<ElementHandle> SearchInput { get; }
+
+        public async Task<GitHubSearchPage> Search(string text)
+        {
+            var input = await SearchInput;
+            if (await input.IsHiddenAsync()) await Page.ClickAsync(".octicon-three-bars");
+            await input.TypeAsync(text);
+            await input.PressAsync(Key.Enter);
+
+            return await Page.WaitForNavigationAsync<GitHubSearchPage>();
+        }
+    }
+
+    public class GitHubSearchPage : PageObject
+    {
+        [Selector(".repo-list-item")]
+        public virtual Task<ElementHandle[]> Results { get; }
+    }
+    
+    public class GitHubRepoPage : PageObject
+    {
+        [Selector("article > h1")]
+        public virtual Task<ElementHandle> Heading { get; }
+        
+        [Selector("img[alt='Build status']")]
+        public virtual Task<ElementHandle> BuildStatusBadge { get; }
+
+        public async Task<string> GetLatestReleaseVersion()
+        {
+            var releases = await Page.QuerySelectorWithContentAsync("a", "releases");
+            await releases.ClickAsync();
+            await Page.WaitForNavigationAsync();
+
+            var latest = await Page.QuerySelectorAsync(".release .release-header a");
+            return VersionWithoutPatch(await latest.TextContentAsync());
+
+            string VersionWithoutPatch(string version)
+            {
+                var tokens = version.Split(".".ToCharArray());
+                return string.Join(".", tokens.Take(2));
+            }
+        }
+    }
+    
+    public class AppVeyorBuildPage : PageObject
+    {
+        public async Task<bool> Success()
+        {
+            var success = await Page.QuerySelectorAsync(".project-build.project-build-status.success");
+            return success.Exists();
+        }
+    }
+}
---
+++ samples/PuppeteerSharp.Contrib.Sample.PageObjects/PuppeteerSharpRepoTests.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using PuppeteerSharp.Contrib.PageObjects;
+using PuppeteerSharp.Contrib.Should;
+using Shouldly;
+
+namespace PuppeteerSharp.Contrib.Sample
+{
+    public class PuppeteerSharpRepoTests
+    {
+        private Browser Browser { get; set; }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            await new BrowserFetcher().DownloadAsync(BrowserFetcher.DefaultRevision);
+            Browser = await Puppeteer.LaunchAsync(new LaunchOptions
+            {
+                Headless = true
+            });
+        }
+
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Browser.CloseAsync();
+        }
+
+        [Test]
+        public async Task Should_be_first_search_result_on_GitHub()
+        {
+            var page = await Browser.NewPageAsync();
+
+            var startPage = await page.GoToAsync<GitHubStartPage>("https://github.com/");
+            startPage.Heading.ShouldHaveContent("Built for developers");
+
+            var headerMenu = await startPage.HeaderMenu;
+            var searchPage = await headerMenu.Search("Puppeteer Sharp");
+
+            var repositories = await searchPage.Results;
+            repositories.Length.ShouldBeGreaterThan(0);
+            var repository = repositories.First();
+            var link = await repository.QuerySelectorAsync("a");
+            var text = await repository.QuerySelectorAsync("p");
+            await repository.ShouldHaveContentAsync("hardkoded/puppeteer-sharp");
+            await text.ShouldHaveContentAsync("Headless Chrome .NET API");
+            await link.ClickAsync();
+
+            var repoPage = await page.WaitForNavigationAsync<GitHubRepoPage>();
+            repoPage.Heading.ShouldHaveContent("Puppeteer Sharp");
+            repoPage.Page.Url.ShouldBe("https://github.com/hardkoded/puppeteer-sharp");
+        }
+
+        [Test]
+        public async Task Should_have_successful_build_status()
+        {
+            var page = await Browser.NewPageAsync();
+
+            var repoPage = await page.GoToAsync<GitHubRepoPage>("https://github.com/hardkoded/puppeteer-sharp");
+            var badge = await repoPage.BuildStatusBadge;
+            await badge.ClickAsync();
+
+            var buildPage = await page.WaitForNavigationAsync<AppVeyorBuildPage>(new NavigationOptions { WaitUntil = new[] { WaitUntilNavigation.Networkidle0 } });
+            var success = await buildPage.Success();
+            success.ShouldBeTrue();
+        }
+
+        [Test]
+        public async Task Should_be_up_to_date_with_the_Puppeteer_version()
+        {
+            var page = await Browser.NewPageAsync();
+
+            var repoPage = await page.GoToAsync<GitHubRepoPage>("https://github.com/hardkoded/puppeteer-sharp");
+            var puppeteerSharpVersion = await repoPage.GetLatestReleaseVersion();
+
+            repoPage = await page.GoToAsync<GitHubRepoPage>("https://github.com/GoogleChrome/puppeteer");
+            var puppeteerVersion = await repoPage.GetLatestReleaseVersion();
+
+            puppeteerSharpVersion.ShouldBe(puppeteerVersion);
+        }
+    }
+}
