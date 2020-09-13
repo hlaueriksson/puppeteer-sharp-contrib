e358e796682524c99fa114ae2bb061de1ef064d2
Henrik Lau Eriksson <henrik.lau.eriksson@gmail.com>
2020-09-13 20:01:24 +0200

    :hammer:

--- nuget-local.bat
+++ nuget-local.bat
@@ -1,2 +1,5 @@
 nuget init .\src\PuppeteerSharp.Contrib.Extensions\bin\Release\ .\packages
+nuget init .\src\PuppeteerSharp.Contrib.Extensions.Unsafe\bin\Release\ .\packages
+nuget init .\src\PuppeteerSharp.Contrib.PageObjects\bin\Release\ .\packages
 nuget init .\src\PuppeteerSharp.Contrib.Should\bin\Release\ .\packages
+nuget init .\src\PuppeteerSharp.Contrib.Should.Unsafe\bin\Release\ .\packages
