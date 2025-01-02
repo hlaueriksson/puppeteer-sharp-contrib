a6165af29bf79228a673840489bc018fd52bda9a
George Volsky <rextextau@gmail.com>
2025-01-02 16:32:23 +0300

    [+] timeout parameter in WaitForElementsRemovedFromDOMAsync IFrame extension method

--- src/PuppeteerSharp.Contrib.Extensions/IFrameExtensions.cs
+++ src/PuppeteerSharp.Contrib.Extensions/IFrameExtensions.cs
@@ -14,12 +14,15 @@ namespace PuppeteerSharp.Contrib.Extensions
         /// </summary>
         /// <param name="iframe">A <see cref="IFrame"/>.</param>
         /// <param name="selector">A selector to query iframe for.</param>
+        /// <param name="timeout">Maximum time to wait for in milliseconds. Pass 0 to disable timeout. Pass null to use default timeout.</param>
         /// <returns>A <see cref="Task"/> representing the result of the asynchronous operation.</returns>
-        public static async Task WaitForElementsRemovedFromDOMAsync(this IFrame iframe, string selector)
+        public static async Task WaitForElementsRemovedFromDOMAsync(this IFrame iframe, string selector, int? timeout = null)
         {
+            var options = new WaitForFunctionOptions { Polling = WaitForFunctionPollingOption.Mutation };
+            if (timeout.HasValue) options.Timeout = timeout;
             await iframe.GuardFromNull().WaitForFunctionAsync(
                 string.Format("async () => document.querySelector('{0}') === null", selector),
-                new WaitForFunctionOptions { Polling = WaitForFunctionPollingOption.Mutation })
+                options)
                 .ConfigureAwait(false);
         }
     }
